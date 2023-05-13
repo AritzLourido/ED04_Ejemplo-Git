5558b337a01466678d11a67868cc07de588a3fe2
Aritz Lourido <arlourido@birt.eus>
2023-05-13 20:00:22 +0200

    Version 0. Primer commit del proyecto.

---
+++ src/main/java/birt/ud04ejemplogit/UD04EjemploGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package birt.ud04ejemplogit;
+
+/**
+ *
+ * @author aritz
+ */
+public class UD04EjemploGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
