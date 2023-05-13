a9e5c75c316c27762e65cb01cdce7785b0fb160f
Aritz Lourido <arlourido@birt.eus>
2023-05-13 20:03:20 +0200

    Version 1.  Commit con cambios básicos

--- src/main/java/birt/ud04ejemplogit/UD04EjemploGit.java
+++ src/main/java/birt/ud04ejemplogit/UD04EjemploGit.java
@@ -1,16 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package birt.ud04ejemplogit;
 
 /**
  *
- * @author aritz
+ * @author aritz lourido
  */
 public class UD04EjemploGit {
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
         System.out.println("Hello World!");
     }
 }
